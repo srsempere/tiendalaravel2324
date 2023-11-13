d4051d5a299d3a96a9a7fa5a91d9ea45d58509b9
Samu <samueljose.garcia@iesdonana.org>
2023-11-13 22:58:19 +0100

    Creado controlador con CRUD automático, modelo y vista de Categoria

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,4 +32,7 @@ Route::middleware('auth')->group(function () {
 Route::get('/samu', function() {
     return view('samu', ['name' => 'Baloo']);
 });
+
+Route::resource('categoria', CategoriaController::class);
+
 require __DIR__.'/auth.php';
