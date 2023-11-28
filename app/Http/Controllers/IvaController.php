a2e6da435d0841815b8246c1916a9fda33096b18
Samu <samueljose.garcia@iesdonana.org>
2023-11-28 13:57:06 +0100

    Index de iva terminado. Mostrando distintos tipos de IVA

--- app/Http/Controllers/IvaController.php
+++ app/Http/Controllers/IvaController.php
@@ -12,7 +12,9 @@ class IvaController extends Controller
      */
     public function index()
     {
-        return view('ivas.index');
+        return view('ivas.index', [
+            'ivas' => Iva::all(),
+        ]);
     }
 
     /**
--- resources/views/ivas/index.blade.php
+++ resources/views/ivas/index.blade.php
@@ -1,3 +1,53 @@
 <x-app-layout>
-Hola, soy el index.
+    <br>
+    <div class="overflow-x-auto">
+        <table class="w-auto mx-auto text-sm text-center text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Tipo
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Porcentaje
+                    </th>
+                    <th scope="col" class="px-6 py-3" colspan="2">
+                        Acción
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($ivas as $iva)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                            {{ $iva->tipo }}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                            {{ $iva->por }}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                            <a href="{{ route('ivas.edit', ['iva' => $iva]) }}">
+                                <x-primary-button>
+                                    Editar
+                                </x-primary-button>
+                            </a>
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                            <form method="post" action="{{ route('ivas.destroy', ['iva' => $iva]) }}">
+                                @csrf
+                                @method('DELETE')
+                                <x-primary-button class="bg-red-700">
+                                    Borrar
+                                </x-primary-button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="flex justify-center">
+        <form action="{{ route('ivas.create') }}" method="get">
+            <x-primary-button class="bg-green-700 m-4">Crear IVA</x-primary-button>
+        </form>
+    </div>
 </x-app-layout>
