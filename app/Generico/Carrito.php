08abe4a03eacb8c06ddd954b0bcfa6934a233f6f
Samuel Garcia <samueljose.garcia@iesdonana.org>
2023-12-05 18:07:24 +0100

    Guardando estado

--- app/Generico/Carrito.php
+++ app/Generico/Carrito.php
@@ -27,18 +27,35 @@ class Carrito
         }
     }
 
-    public function eliminar()
+    public function eliminar($id)
     {
-        //
+        if (!isset($this->lineas[$id])) {
+            throw new ValueError('Artículo inexistente en el carrito');
+        }
+
+        $this->lineas[$id]->decCantidad();
+        if ($this->lineas[$id]->getCantidad() == 0) {
+            unset($this->lineas[$id]);
+        }
     }
 
     public function vacio()
     {
-        //
+        return empty($this->lineas);
     }
 
     public function getLineas()
     {
-        //
+        return $this->lineas;
+    }
+
+
+    public static function carrito()
+    {
+        if (session()->missing('carrito')) {
+            session()->put('carrito', new static());
+        }
+
+        return session('carrito');
     }
 }
