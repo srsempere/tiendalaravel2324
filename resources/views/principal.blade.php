49e405491685dc8e1664b9c471550bbdb8f7f891
Samuel García <samueljose.garcia@iesdonana.org>
2023-12-06 12:09:32 +0100

    Actualización página principal: Se muestran los precios y las categorías de los artículos

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -4,8 +4,10 @@
             @foreach ($articulos as $articulo)
                 <a href="#"
                     class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {{ $articulo->denominacion }}</h5>
+                    <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        {{ $articulo->denominacion }}</h1>
+                    <p>Precio: {{ dinero($articulo->precio) }}</p>
+                    <p>Categoria: {{ $articulo->categoria->nombre }}</p>
                     <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium aspernatur rem fuga similique et porro amet voluptatem asperiores, molestiae veritatis voluptatum ipsum, ipsam, magni possimus repellendus in quam quod suscipit.</p>
                 </a>
             @endforeach
